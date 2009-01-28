e0b8a08a2dbd1157ec904ce9a217b5a6320d9e58
Josh Starcher <josh.starcher@gmail.com>
2009-01-28 04:02:47 -0600

    add ability to join/leave a project

--- app/controllers/project_members_controller.rb
+++ app/controllers/project_members_controller.rb
@@ -6,6 +6,19 @@ class ProjectMembersController < ApplicationController
     @project_member = @project.project_members.new
   end
   
+  def create
+    unless@project.users.include?(current_user)
+      @project.project_members.create(:user_id => current_user.id, :skills => params[:skills], :role => 'Member')
+    end
+    redirect_to(@project)
+  end
+  
+  def destroy
+    @project_member = ProjectMember.find(params[:id])
+    @project_member.destroy if (current_user == @project.creator || current_user == @project_member.user) && @project_member.role != 'Owner'
+    redirect_to(@project)
+  end
+  
   protected
   def get_project
     @project = Project.find(params[:project_id])
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   self.inheritance_column = 'false'
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
-  has_many :project_members, :order => 'role desc'
+  has_many :project_members, :order => 'role desc, created_at', :dependent => :destroy
   has_many :users, :through => :project_members
   
   def seeking_volunteers_human
