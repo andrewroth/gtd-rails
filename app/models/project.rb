72c23a1f4d6f239cd379bbb8dace55b78c6edc6c
Andrew Roth <andrewroth@gmail.com>
2009-01-28 20:31:21 -0800

    add validation for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,9 @@ class Project < ActiveRecord::Base
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
   has_many :project_members, :order => 'role desc, created_at', :dependent => :destroy
   has_many :users, :through => :project_members
+  validates_presence_of :status, :skills_needed, :name, :purpose, :scope, :creator_id, 
+                        :type, :resources_needed, :hosting_home, :estimated_days, :website,
+                        :description, :is_seeking_volunteers, :active
   
   def seeking_volunteers_human
     is_seeking_volunteers? ? 'Yes' : 'No'
